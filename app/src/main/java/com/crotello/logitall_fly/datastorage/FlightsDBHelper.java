2bd5638a95b51b9181b79875197fcd6c4670669d
Timokawa <tim@crotello.com>
2017-01-06 12:32:31 +0000

    Add of design documents. Restructure of architecture.

---
+++ app/src/main/java/com/crotello/logitall_fly/datastorage/FlightsDBHelper.java
@@ -0,0 +1,63 @@
+package com.crotello.logitall_fly.datastorage;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.crotello.logitall_fly.datastorage.FlightsContract.FlightEntry;
+
+
+/**
+ * Created by Tim on 10/09/2016.
+ */
+
+
+public class FlightsDBHelper extends SQLiteOpenHelper {
+
+    static final int DB_VERSION = 2;
+
+    // Database Information
+    static final String DB_NAME = "flights.db";
+
+    public FlightsDBHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        // Create table query
+         final String CREATE_FLIGHT_TABLE =
+                "create table "
+                        + FlightEntry.TABLE_NAME + "("
+                        + FlightEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                        + FlightEntry.COLUMN_FLIGHT_NUMBER + " TEXT, "
+                        + FlightEntry.COLUMN_DEPARTURE_DATE_AND_TIME + " INTEGER NOT NULL, "
+                        + FlightEntry.COLUMN_ARRIVAL_DATE_AND_TIME + " INTEGER NOT NULL, "
+                        + FlightEntry.COLUMN_NIGHT_HOURS + " INTEGER NOT NULL, "
+                        + FlightEntry.COLUMN_DAY_HOURS + " INTEGER NOT NULL, "
+                        + FlightEntry.COLUMN_TOTAL_HOURS + " INTEGER NOT NULL,"
+                        + FlightEntry.COLUMN_ROLE + " INTEGER NOT NULL,"
+                        + FlightEntry.COLUMN_PIC + " INTEGER NOT NULL,"
+                        + FlightEntry.COLUMN_ICAO_Departure + " INTEGER NOT NULL,"
+                        + FlightEntry.COLUMN_ICAO_Destination + " INTEGER NOT NULL,"
+                        + FlightEntry.COLUMN_Aircraft_Type + " INTEGER NOT NULL,"
+                        + FlightEntry.COLUMN_Aircraft_Number + " INTEGER NOT NULL);";
+
+        sqLiteDatabase.execSQL(CREATE_FLIGHT_TABLE);
+
+    }
+
+    public static final String COLUMN_ICAO_Destination = "ICAO_Destination";
+    public static final String COLUMN_ICAO_Departure = "ICAO_Departure";
+
+    // Deleting the table
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + FlightEntry.TABLE_NAME;
+
+    public void onUpgrade(SQLiteDatabase database,
+                          int oldVersion, int newVersion) {
+
+        database.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(database);
+    }
+}
